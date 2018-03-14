f217490a6f85fe14ebc0f02be42b6989b7b34e0a
Igor Vasilcovsky <vasilcovsky@gmail.com>
2018-03-14 06:43:14 -0400

    Initial commit

---
+++ botframework.py
@@ -0,0 +1,249 @@
+import json
+import logging
+import datetime
+import requests
+
+from time import sleep
+from urllib.parse import urljoin
+from collections import namedtuple
+
+from bottle import request
+from errbot.errBot import ErrBot
+from errbot.core_plugins.wsview import bottle_app
+from errbot.backends.base import Message
+
+log = logging.getLogger('errbot.backends.botframework')
+authtoken = namedtuple('AuthToken', 'access_token, expired_at')
+activity = namedtuple('Activity', 'post_url, payload')
+
+
+def from_now(seconds):
+    now = datetime.datetime.now()
+    return now + datetime.timedelta(seconds=seconds)
+
+
+def auth(appId, appPasswd):
+    form = {
+        'grant_type': 'client_credentials',
+        'scope': 'https://api.botframework.com/.default',
+        'client_id': appId,
+        'client_secret': appPasswd,
+    }
+
+    r = requests.post(
+        'https://login.microsoftonline.com/botframework.com/oauth2/v2.0/token',
+        data=form
+    ).json()
+
+    expires_in = r['expires_in']
+    expired_at = from_now(expires_in)
+    token = authtoken(r['access_token'], expired_at)
+
+    return token
+
+
+class Conversation:
+    """ Wrapper on Activity object.
+
+    See more:
+        https://docs.microsoft.com/en-us/bot-framework/rest-api/bot-framework-rest-connector-api-reference#activity-object
+    """
+
+    def __init__(self, conversation):
+        self._conversation = conversation
+
+    @property
+    def conversation(self):
+        return self._conversation['conversation']
+
+    @property
+    def conversation_id(self):
+        return self.conversation['id']
+
+    @property
+    def activity_id(self):
+        return self._conversation['id']
+
+    @property
+    def service_url(self):
+        return self._conversation['serviceUrl']
+
+    @property
+    def reply_url(self):
+        url = '/v3/conversations/{}/activities/{}'.format(
+            self.conversation_id,
+            self.activity_id
+        )
+
+        return urljoin(self.service_url, url)
+
+
+class Identifier:
+    def __init__(self, subject):
+        self._subject = subject
+        self._id = subject.get('id', '<not found>')
+        self._name = subject.get('name', '<not found>')
+
+    def __str__(self):
+        return '[' + self._id + '] ' + self._name
+
+    @property
+    def subject(self):
+        return self._subject
+
+    @property
+    def userid(self):
+        return self._id
+
+    @property
+    def aclattr(self):
+        return self._id
+
+    @property
+    def person(self):
+        return self._name
+
+    @property
+    def nick(self):
+        return self._name
+
+    @property
+    def fullname(self):
+        return self._name
+
+    @property
+    def client(self):
+        return '<not set>'
+
+
+class BotFramework(ErrBot):
+    """Errbot Backend for Bot Framework"""
+
+    def __init__(self, config):
+        super(BotFramework, self).__init__(config)
+
+        identity = config.BOT_IDENTITY
+        self._appId = identity.get('appId', '')
+        self._appPassword = identity.get('appPassword', '')
+        self._token = None
+
+    def _ensure_token(self):
+        """Keep OAuth token valid"""
+        now = datetime.datetime.now()
+        if not self._token or self._token.expired_at <= now:
+            self._token = auth(self._appId, self._appPassword)
+        return self._token.access_token
+
+    def _build_reply(self, msg):
+        conversation = msg.extras['conversation']
+        payload = {
+            'type': 'message',
+            'conversation': conversation.conversation,
+            'from': msg.to.subject,
+            'recipient': msg.frm.subject,
+            'replyToId': conversation.conversation_id,
+            'text': msg.body
+        }
+        return activity(conversation.reply_url, payload)
+
+    def _build_feedback(self, msg):
+        conversation = msg.extras['conversation']
+        payload = {
+            'type': 'typing',
+            'conversation': conversation.conversation,
+            'from': msg.to.subject,
+            'replyToId': conversation.conversation_id,
+        }
+        return activity(conversation.reply_url, payload)
+
+    def _send_reply(self, response):
+        """Post response to callback url
+
+        Send a reply to URL indicated in serviceUrl from
+        Bot Framework's request object.
+
+        @param response: activity object
+        """
+        access_token = self._ensure_token()
+
+        headers = {
+            'Authorization': 'Bearer ' + access_token,
+            'Content-Type': 'application/json'
+        }
+
+        r = requests.post(
+            response.post_url,
+            data=json.dumps(response.payload),
+            headers=headers
+        )
+
+        r.raise_for_status()
+
+    def serve_forever(self):
+        self._init_handler(self)
+        self.connect_callback()
+
+        try:
+            while True:
+                sleep(1)
+        except KeyboardInterrupt:
+            log.info("Interrupt received, shutting down")
+        finally:
+            self.disconnect_callback()
+            self.shutdown()
+
+    def send_message(self, msg):
+        response = self._build_reply(msg)
+        self._send_reply(response)
+        super(BotFramework, self).send_message(msg)
+
+    def build_identifier(self, user):
+        return Identifier(user)
+
+    def build_reply(self, msg, text=None, private=False, threaded=False):
+        return Message(
+            body=text,
+            parent=msg,
+            frm=msg.frm,
+            to=msg.to,
+            extras=msg.extras,
+        )
+
+    def send_feedback(self, msg):
+        feedback = self._build_feedback(msg)
+        self._send_reply(feedback)
+
+    def build_conversation(self, conv):
+        return Conversation(conv)
+
+    def change_presence(self, status, message):
+        pass
+
+    def query_room(self, room):
+        return None
+
+    def rooms(self):
+        return []
+
+    @property
+    def mode(self):
+        return 'BotFramework'
+
+    def _init_handler(self, errbot):
+        @bottle_app.route('/botframework', method=['GET', 'OPTIONS'])
+        def get_botframework():
+            pass
+
+        @bottle_app.route('/botframework', method=['POST'])
+        def post_botframework():
+            req = request.json
+            log.debug('received request: type=[%s] channel=[%s]',
+                      req['type'], req['channelId'])
+            if req['type'] == 'message':
+                msg = Message(req['text'])
+                msg.frm = errbot.build_identifier(req['from'])
+                msg.to = errbot.build_identifier(req['recipient'])
+                msg.extras['conversation'] = errbot.build_conversation(req)
+
+                errbot.send_feedback(msg)
+                errbot.callback_message(msg)
